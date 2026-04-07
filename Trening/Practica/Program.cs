e17bccbd5940963dd653954666c221bf8cb7a761
Dmitrii <47553263+Dmitrii-Aleksei4@users.noreply.github.com>
2026-04-07 10:55:17 +0400

    Add files via upload

---
+++ Trening/Practica/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Practica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(primer(3));
+            Console.WriteLine(primer(3,5));
+            Console.WriteLine(primer(3,5,4));
+            //primer(3,5,6);
+        }
+
+        static int primer (int a)
+        {
+            return a;
+
+        }
+
+        static int primer(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+
+        }
+
+        //static void primer(params int[] vse_int)
+        //{
+        //    foreach (int vse in vse_int)
+        //    {
+        //        Console.WriteLine(vse);
+                
+        //    }
+            
+        //}
+
+        static int primer(params int[] vse_int)
+        {
+            foreach (int vse in vse_int)
+            {
+                Console.WriteLine(vse);
+                
+            }
+            return 0;
+        }
+
+    }
+}
